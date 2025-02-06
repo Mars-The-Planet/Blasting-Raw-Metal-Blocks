7a6e2d31a84b3a534964d266db13ea6a83d31cb3
Matej <ruzicka.maty@gmail.com>
2025-02-06 16:30:18 +0100

    backported to 1.20.4

---
+++ common/src/main/java/com/mars/blastingraw/CommonClass.java
@@ -0,0 +1,19 @@
+package com.mars.blastingraw;
+
+import com.mars.deimos.config.DeimosConfig;
+import com.mars.deimos.datagen.DeimosRecipeGenerator;
+
+import static com.mars.blastingraw.Constants.MOD_ID;
+
+public class CommonClass {
+    public static void init() {
+        DeimosConfig.init(MOD_ID, BlastingRawConfig.class);
+
+        for(String rawMetal : BlastingRawConfig.raw_metal_list){
+            String[] rawMetalSet =  ((rawMetal).replaceAll("\\s","")).split(",");
+            DeimosRecipeGenerator.createBlastingJson(rawMetalSet[0], rawMetalSet[1], BlastingRawConfig.cooking_time, Float.parseFloat(rawMetalSet[2]));
+            if(BlastingRawConfig.works_in_furnace)
+                DeimosRecipeGenerator.createSmeltingJson(rawMetalSet[0], rawMetalSet[1], BlastingRawConfig.cooking_time * 2, Float.parseFloat(rawMetalSet[2]));
+        }
+    }
+}
---
+++ common/src/main/java/com/mars/blastingraw/Constants.java
@@ -0,0 +1,11 @@
+package com.mars.blastingraw;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "blastingraw";
+    public static final String MOD_NAME = "Blasting Raw Metal Blocks";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
