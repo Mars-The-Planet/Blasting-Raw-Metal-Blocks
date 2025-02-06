c64cc8de71f14db608996879efbb33e23d3434df
Matej <ruzicka.maty@gmail.com>
2025-02-06 16:23:48 +0100

    backported to 1.20.6

---
+++ common/src/main/java/com/mars/blastingraw/BlastingRawConfig.java
@@ -0,0 +1,16 @@
+package com.mars.blastingraw;
+
+import com.google.common.collect.Lists;
+import com.mars.deimos.config.DeimosConfig;
+
+import java.util.List;
+
+public class BlastingRawConfig extends DeimosConfig {
+    @Entry public static int cooking_time = 100;
+    @Entry public static List<String> raw_metal_list = Lists.newArrayList(
+            "minecraft:raw_copper_block, minecraft:copper_block, 6.3",
+            "minecraft:raw_iron_block, minecraft:iron_block, 6.3",
+            "minecraft:raw_gold_block, minecraft:gold_block, 9"
+    );
+    @Entry public static boolean works_in_furnace = false;
+}
---
+++ forge/src/main/java/com/mars/blastingraw/BlastingRaw.java
@@ -0,0 +1,10 @@
+package com.mars.blastingraw;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class BlastingRaw {
+    public BlastingRaw() {
+        CommonClass.init();
+    }
+}
